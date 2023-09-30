3d5de24edbdeef9bed5e705a25572b756fcd5ffa
andreyd05 <andreyd@iastate.edu>
2023-09-30 16:42:22 -0500

    aa

--- Agriventure/index.js
+++ Agriventure/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+function changeColor() {
+    let abc = document.getElementById("abc");
+    let red = document.getElementById("redSlider").value;
+    let green = document.getElementById("greenSlider").value;
+    let blue = document.getElementById("blueSlider").value;
+    abc.style.color = `rgb(${red}, ${green}, ${blue})`;
+    console.log(red);
+}
